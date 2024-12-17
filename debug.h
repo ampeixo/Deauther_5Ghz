f8eaffd4934ad836df69477f07b4a7c548d3815f
Alberto Magno <58338530+ampeixo@users.noreply.github.com>
2024-12-17 18:11:20 -0300

    Refactoring debug.h

--- debug.h
+++ debug.h
@@ -3,15 +3,30 @@
 
 #include <Arduino.h>
 
+// === DEBUG CONFIGURATION ===
+// Uncomment the line below to enable debug mode
 //#define DEBUG
+
 #define DEBUG_BAUD 115200
 
+// === DEBUG MACROS ===
 #ifdef DEBUG
-  #define DEBUG_SER_INIT() Serial.begin(DEBUG_BAUD);
-  #define DEBUG_SER_PRINT(...) Serial.print(__VA_ARGS__);
+  // Initialize Serial only if in debug mode
+  #define DEBUG_SER_INIT()      \
+    do {                        \
+      if (!Serial) {            \
+        Serial.begin(DEBUG_BAUD); \
+      }                         \
+    } while (0)
+
+  // Print data to the serial monitor
+  #define DEBUG_SER_PRINT(...)  Serial.print(__VA_ARGS__)
+  #define DEBUG_SER_PRINTLN(...) Serial.println(__VA_ARGS__)
 #else
-  #define DEBUG_SER_PRINT(...)
+  // Disable all debug calls
   #define DEBUG_SER_INIT()
+  #define DEBUG_SER_PRINT(...)
+  #define DEBUG_SER_PRINTLN(...)
 #endif
 
-#endif
+#endif // DEBUG_H
